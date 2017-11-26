e0431f74daaeaa56c3813df8952638bfeea14390
skylics <skylics@naver.com>
2017-11-26 17:56:22 +0900

    fix things

--- R/plot_graph.R
+++ R/plot_graph.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' plotfn(trade_data, date, y_plot[i])
-plotfn <- function(data, xvar, yvar){
+plotfn <- function(data, xvar, yvar) {
 
   data_gd <- NULL
 
@@ -41,47 +41,4 @@ plotfn <- function(data, xvar, yvar){
              y = max(data_gd$yvar),
              hjust = 0.1,
              label = y_unit[i])
-}
-
-#' Create multi ggplot graphes (code origin - http://www.cookbook-r.com/Graphs)
-#'
-#' @return a multiplot
-#' @export
-#'
-#' @examples
-#' multiplot(p_1, p_2, p_3, p_4, cols = 2)
-multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL) {
-  library(grid)
-
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-
-  numPlots = length(plots)
-
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-
-  if (numPlots==1) {
-    print(plots[[1]])
-
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
 }
