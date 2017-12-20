dfc53677d0f5eaf2ba8873e67f69020c7d579bc4
skylics <skylics@naver.com>
2017-12-20 15:48:19 +0900

    fix

--- R/daily_candle.R
+++ R/daily_candle.R
@@ -1,194 +1,194 @@
-library(dplyr)
-library(parallel)
-library(tictoc)
-library(profvis)
-library(htmlwidgets)
-
-
-##############################
-############### º¯¼ö ¼³Á¤
-
-## ÄÚ¾î¼ö °¨Áö
-(core_num <- parallel::detectCores(all.tests = FALSE, logical = TRUE))
-
-
-## µ¥ÀÌÅÍ load
-setwd("P:/LICS/Rwd/Stock_analysis/data")
-load("all_stocks.rda")
-
-
-## INPUT data - X »ý¼º 
-all_stocks$code %>% as.data.frame(stringsAsFactors = FALSE) ->.;
-#.[c(1:nrow(.)), ] -> datax
-.[c(1:10), ] -> datax
-
-
-## INDEX data - z »ý¼º 
-zlevels <- 1:core_num
-dataz <- if (sum(1:(length(datax) %% core_num)) == 1) {
-  rep(1:core_num, length(datax) %/% core_num)
-} else {
-  c(rep(1:core_num, length(datax) %/% core_num), 1:(length(datax) %% core_num))
-}
-
-
-##############################
-############### ÇÔ¼ö ¼³Á¤
-
-
-###1### WRAPPER ÇÔ¼ö »ý¼º
-wrapper <- function(datax, dataz) {
-  
-  
-  force(datax)
-  force(dataz)
-  
-  
-  ###2### ³»¿ë ÇÔ¼ö »ý¼º
-  activation <- function(zlevel, datax, dataz) {
-    
-    
-    ###3### Attach packages
-    library(rebus, quietly = TRUE)
-    library(rvest, quietly = TRUE)
-    
-    
-    ###3### Define variables
-    code_part <- datax[dataz == zlevel]
-    
-    
-    ###3### Function body
-    real_time <- function(ticker) {
-      
-      ticker ->.;
-      paste("http://finance.daum.net/item/quote.daum?code=", ., sep = "") ->.;
-      read_html(., encoding = "UTF-8") ->.;
-      html_nodes(., "body") ->.;
-      html_nodes(., "div") ->.;
-      html_nodes(., ".leftDiv") ->.;
-      html_nodes(., "td") ->.;
-      html_text(.) %>%
-        (function(df) {
-          data.frame("Open" = df[which(df == "½Ã°¡") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
-                     "High" = df[which(df == "°í°¡") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
-                     "Low" = df[which(df == "Àú°¡") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
-                     "Close" = df[which(df == "ÇöÀç°¡") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
-                     "Volumn" = df[which(df == "°Å·¡·®") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric
-                     ) %>% xts::xts(order.by = Sys.Date())
-        }) %>%
-        (function(df) {
-          df %>% apply(1, scale)
-          
-        })
-    }
-    
-    
-    ###3### Activate the function with loop using map()
-    code_part ->.; 
-    purrr::map(., function(ticker) {
-      
-      real_time(ticker)
-      
-    }) ###3### Last line of code_part
-    
-  } ###2### Last line of Activation function()
-  
-  
-  ####2####
-  worker <- function(zlevel) {activation(zlevel, datax, dataz)}
-  
-  return(worker)
-
-} ###1### Last line of price
-
-
-##############################
-############### ÄÚ¾î È°¿ë Scrape ½ÃÇà + ½Ã°£ Ã¼Å©
-
-tic("ÀüÃ¼½Ã°£")
-parallelcluster <- parallel::makeCluster(parallel::detectCores())
-
-tic("10°³ Å©·Ñ¸µ")
-models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
-toc()
-
-parallel::stopCluster(parallelcluster)
-toc()
-
-
-##############################
-############### ÀúÀåÇÏ±â 
-
-###1### Export models data
-
-save(models, file = "price_scrape.rda")
-
-# tic("ÀúÀå½Ã°£")
-# 
-# setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
-# Sys.setenv("R_ZIPCMD" = "C:/Rtools/bin/zip.exe")
-# 
-# for (j in 1:core_num) {
-#   
-#   core_codes <- datax[dataz == j]
-#   core_nms <- all_stocks$name[dataz == j]
-#   
-#   for (i in 1:length(core_nms)) {
-#     rm(wb)
-#     wb <- openxlsx::createWorkbook() 
-#     addWorksheet(wb, core_codes[i])
-#     writeData(wb, 
-#               sheet = core_codes[i], 
-#               models[[j]][[i]] %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column("date"),
-#               startCol = 1, startRow = 1)
-#     openxlsx::saveWorkbook(wb, paste(core_nms[i], ".xlsx", sep = ""), overwrite = TRUE)
-#   }
+# library(dplyr)
+# library(parallel)
+# library(tictoc)
+# library(profvis)
+# library(htmlwidgets)
+#
+#
+# ##############################
+# ############### ???? ??ï¿½ï¿½
+#
+# ## ?Ú¾??? ????
+# (core_num <- parallel::detectCores(all.tests = FALSE, logical = TRUE))
+#
+#
+# ## ?????? load
+# setwd("P:/LICS/Rwd/Stock_analysis/data")
+# load("all_stocks.rda")
+#
+#
+# ## INPUT data - X ????
+# all_stocks$code %>% as.data.frame(stringsAsFactors = FALSE) ->.;
+# #.[c(1:nrow(.)), ] -> datax
+# .[c(1:10), ] -> datax
+#
+#
+# ## INDEX data - z ????
+# zlevels <- 1:core_num
+# dataz <- if (sum(1:(length(datax) %% core_num)) == 1) {
+#   rep(1:core_num, length(datax) %/% core_num)
+# } else {
+#   c(rep(1:core_num, length(datax) %/% core_num), 1:(length(datax) %% core_num))
 # }
-# 
+#
+#
+# ##############################
+# ############### ?Ô¼? ??ï¿½ï¿½
+#
+#
+# ###1### WRAPPER ?Ô¼? ????
+# wrapper <- function(datax, dataz) {
+#
+#
+#   force(datax)
+#   force(dataz)
+#
+#
+#   ###2### ???? ?Ô¼? ????
+#   activation <- function(zlevel, datax, dataz) {
+#
+#
+#     ###3### Attach packages
+#     library(rebus, quietly = TRUE)
+#     library(rvest, quietly = TRUE)
+#
+#
+#     ###3### Define variables
+#     code_part <- datax[dataz == zlevel]
+#
+#
+#     ###3### Function body
+#     real_time <- function(ticker) {
+#
+#       ticker ->.;
+#       paste("http://finance.daum.net/item/quote.daum?code=", ., sep = "") ->.;
+#       read_html(., encoding = "UTF-8") ->.;
+#       html_nodes(., "body") ->.;
+#       html_nodes(., "div") ->.;
+#       html_nodes(., ".leftDiv") ->.;
+#       html_nodes(., "td") ->.;
+#       html_text(.) %>%
+#         (function(df) {
+#           data.frame("Open" = df[which(df == "?Ã°?") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
+#                      "High" = df[which(df == "?ï¿½ï¿½ï¿½") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
+#                      "Low" = df[which(df == "????") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
+#                      "Close" = df[which(df == "???ç°¡") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric,
+#                      "Volumn" = df[which(df == "?Å·???") + 1] %>% stringr::str_replace_all(",", "") %>% as.numeric
+#                      ) %>% xts::xts(order.by = Sys.Date())
+#         }) %>%
+#         (function(df) {
+#           df %>% apply(1, scale)
+#
+#         })
+#     }
+#
+#
+#     ###3### Activate the function with loop using map()
+#     code_part ->.;
+#     purrr::map(., function(ticker) {
+#
+#       real_time(ticker)
+#
+#     }) ###3### Last line of code_part
+#
+#   } ###2### Last line of Activation function()
+#
+#
+#   ####2####
+#   worker <- function(zlevel) {activation(zlevel, datax, dataz)}
+#
+#   return(worker)
+#
+# } ###1### Last line of price
+#
+#
+# ##############################
+# ############### ?Ú¾? È°?? Scrape ???? + ?Ã°? Ã¼Å©
+#
+# tic("??Ã¼?Ã°?")
+# parallelcluster <- parallel::makeCluster(parallel::detectCores())
+#
+# tic("10?? Å©?Ñ¸?")
+# models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
 # toc()
-
-
-##############################
-############### ¿¢¼¿ ÆÄÀÏ ºÒ·¯¿À±â
-
-###1### Import models data.xlsx
-
-tic("ÀúÀå½Ã°£")
-
-setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
-dir_nms <- dir()
-
-
-## Create empty models list
-daily_candle_multi <- vector("list", length(dir_nms))
-
-
-## Import excel files
-for (i in 1:length(dir_nms)) {
-  readxl::read_excel(dir_nms[i], col_types = rep("text", 6)) ->.;
-  xts::xts(.[, -1] %>% apply(2, as.numeric), order.by = zoo::as.Date(.$date)) %>%
-    (function(df) {list(code = which(dir_nms[i] %>% stringr::str_replace_all(".xlsx", "") == all_stocks$name) %>% all_stocks$code[.],
-                        price = df)
-    }) -> models[[i]]
-}
-
-names(models) <- dir_nms %>% stringr::str_replace_all(".xlsx", "")
-save(models, file = "price_scrape.rda")
-
-toc()
-
-
-##############################
-############### ÇÁ·ÎÆÄÀÏ¸µ - profvis ÆÐÅ°Áö È°¿ë
-
-# model_profvis <- profvis({
-#   
-#   parallelcluster <- parallel::makeCluster(parallel::detectCores())
-#   
-#   models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
-#   
-#   parallel::stopCluster(parallelcluster)
-#   
-#   })
-# 
-# saveWidget(widget = model_profvis, file = "model_profvis.html")
+#
+# parallel::stopCluster(parallelcluster)
+# toc()
+#
+#
+# ##############################
+# ############### ?????Ï±?
+#
+# ###1### Export models data
+#
+# save(models, file = "price_scrape.rda")
+#
+# # tic("?????Ã°?")
+# #
+# # setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
+# # Sys.setenv("R_ZIPCMD" = "C:/Rtools/bin/zip.exe")
+# #
+# # for (j in 1:core_num) {
+# #
+# #   core_codes <- datax[dataz == j]
+# #   core_nms <- all_stocks$name[dataz == j]
+# #
+# #   for (i in 1:length(core_nms)) {
+# #     rm(wb)
+# #     wb <- openxlsx::createWorkbook()
+# #     addWorksheet(wb, core_codes[i])
+# #     writeData(wb,
+# #               sheet = core_codes[i],
+# #               models[[j]][[i]] %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column("date"),
+# #               startCol = 1, startRow = 1)
+# #     openxlsx::saveWorkbook(wb, paste(core_nms[i], ".xlsx", sep = ""), overwrite = TRUE)
+# #   }
+# # }
+# #
+# # toc()
+#
+#
+# ##############################
+# ############### ???? ???? ?Ò·??ï¿½ï¿½?
+#
+# ###1### Import models data.xlsx
+#
+# tic("?????Ã°?")
+#
+# setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
+# dir_nms <- dir()
+#
+#
+# ## Create empty models list
+# daily_candle_multi <- vector("list", length(dir_nms))
+#
+#
+# ## Import excel files
+# for (i in 1:length(dir_nms)) {
+#   readxl::read_excel(dir_nms[i], col_types = rep("text", 6)) ->.;
+#   xts::xts(.[, -1] %>% apply(2, as.numeric), order.by = zoo::as.Date(.$date)) %>%
+#     (function(df) {list(code = which(dir_nms[i] %>% stringr::str_replace_all(".xlsx", "") == all_stocks$name) %>% all_stocks$code[.],
+#                         price = df)
+#     }) -> models[[i]]
+# }
+#
+# names(models) <- dir_nms %>% stringr::str_replace_all(".xlsx", "")
+# save(models, file = "price_scrape.rda")
+#
+# toc()
+#
+#
+# ##############################
+# ############### ?ï¿½ï¿½????Ï¸? - profvis ??Å°?? È°??
+#
+# # model_profvis <- profvis({
+# #
+# #   parallelcluster <- parallel::makeCluster(parallel::detectCores())
+# #
+# #   models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
+# #
+# #   parallel::stopCluster(parallelcluster)
+# #
+# #   })
+# #
+# # saveWidget(widget = model_profvis, file = "model_profvis.html")
--- R/daily_candle2.R
+++ R/daily_candle2.R
@@ -1,272 +1,272 @@
-library(dplyr)
-library(parallel)
-library(tictoc)
-library(profvis)
-library(htmlwidgets)
-
-
-##############################
-############### 변수 설정
-
-## 코어수 감지
-(core_num <- parallel::detectCores(all.tests = FALSE, logical = TRUE))
-
-
-## 데이터 load
-setwd("P:/LICS/Rwd/Stock_analysis/data")
-#setwd("C:/Users/user/Desktop/R project/Github/Stock_analysis/data")
-load("all_stocks.rda")
-
-
-## INPUT data - X 생성
-all_stocks$code %>% as.data.frame(stringsAsFactors = FALSE) ->.;
-.[c(1:nrow(.)), ] -> datax
-#.[c(1:100), ] -> datax
-
-
-## INDEX data - z 생성
-zlevels <- 1:core_num
-dataz <- if (sum(1:(length(datax) %% core_num)) == 1) {
-  rep(1:core_num, length(datax) %/% core_num)
-} else {
-  c(rep(1:core_num, length(datax) %/% core_num), 1:(length(datax) %% core_num))
-}
-
-
-##############################
-############### 함수 설정
-
-
-###1### WRAPPER 함수 생성
-wrapper <- function(datax, dataz) {
-
-
-  force(datax)
-  force(dataz)
-
-
-  ###2### 내용 함수 생성
-  activation <- function(zlevel, datax, dataz) {
-
-
-    ###3### Attach packages
-    library(rebus, quietly = TRUE)
-    library(rvest, quietly = TRUE)
-
-
-    ###3### Define variables
-    code_part <- datax[dataz == zlevel]
-
-
-    ###3### Function body - real_time()
-    real_time <- function(ticker) {
-
-      ticker ->.;
-      paste("http://finance.daum.net/item/quote.daum?code=", ., sep = "") ->.;
-      read_html(., encoding = "UTF-8") ->.;
-      html_nodes(., "body") ->.;
-      html_nodes(., "div") ->.;
-      html_nodes(., ".leftDiv") ->.;
-      html_nodes(., "td") ->.;
-      html_text(.) %>%
-        (function(df) {c("시가", "고가", "저가", "현재가", "거래량") %>%
-            purrr::map(function(char) {
-              char ->.; df[which(df == .) + 1] ->.; stringr::str_replace_all(., ",", "") ->.; as.numeric(.)
-            })
-        }) ->.;
-      unlist(.) ->.;
-      c(ticker, .)
-
-    } ###3### Last line of real_time()
-
-
-    ###3### Function body - scale_cluster
-    make_scale <- function(df) {
-
-      df[2:5] -> .
-      as.numeric(.) ->.;
-      log(.) ->.;
-
-      if (sum(is.infinite(.)) > 0) {
-        c(0, 0, 0, 0) ->.;
-        c(df[1], ., df[6]) ->.;
-
-      } else if (sum(is.na(.)) > 0) {
-        c(0, 0, 0, 0) ->.;
-        c(paste(df[1], "_X", sep = ""), ., df[6]) ->.;
-
-      } else if (sum(is.infinite(.)) == 0) {
-        (. - .[1]) ->.;
-        c(df[1], ., df[6]) ->.;
-      }
-
-      as.data.frame(., stringsAsFactors = FALSE) ->.;
-      t(.) ->.;
-      {colnames(.) <- c("code", "Open", "High", "Low", "Close", "done_Q"); .} ->.;
-      .
-
-    } ###3### Last line of scale_cluster()
-
-
-    ###3### Function body - scale_kmeans
-    scale_kmeans <- function(df, k) {
-
-      unlist(df, recursive = TRUE) ->.;
-      matrix(., ncol = 6, byrow = TRUE) -> df2
-
-      df2[, c(2:5)] ->.;
-      stats::kmeans(., centers = k, iter.max = 100, nstart = k) ->.;
-
-      cbind(df2[, c(2:5)], .$cluster, df2[, 6]) ->.;
-      {colnames(.) <- c("Open", "High", "Low", "Close", "cluster", "done_Q"); .} ->.;
-      apply(., 2, as.numeric) %>% as.data.frame(stringsAsFactors = FALSE) ->.;
-      {.$cluster <- factor(.$cluster); .} ->.;
-      {rownames(.) <- df2[, 1]; .} ->.;
-      .
-
-    } ###3### Last line of scale_cluster()
-
-
-    ###3### Activate the function with loop using map()
-    code_part ->.;
-    purrr::map(., function(ticker) {
-
-      real_time(ticker) ->.;
-      make_scale(.)
-
-    }) ->.; ###3### Last line of code_part
-
-    scale_kmeans(., 2)
-
-  } ###2### Last line of Activation function()
-
-
-  ####2####
-  worker <- function(zlevel) {activation(zlevel, datax, dataz)}
-
-  return(worker)
-
-} ###1### Last line of price
-
-
-##############################
-############### 코어 활용 Scrape 시행 + 시간 체크
-
-tic("전체시간")
-parallelcluster <- parallel::makeCluster(parallel::detectCores())
-
-tic("10개 크롤링")
-models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
-toc()
-
-parallel::stopCluster(parallelcluster)
-toc()
-
-
-##############################
-############### 저장하기
-
-models %>% purrr::map(function(df) {
-  df %>% tibble::rownames_to_column("code")
-}) %>% rlist::list.flatten() %>% do.call(cbind, .) %>% as.data.frame %>%
-  tibble::rownames_to_column("code") %>% (function(df) {df$num <- c(1:nrow(df)); df})
-
-k <- 8
-cl <- stats::kmeans(df[, c(1:4)], centers = k, iter.max = 100, nstart = k)
-
-par(mfrow = c(2, k/2))
-myCol <- c("blue", "red", "black", "brown", "green", "purple", "pink", "green")
-
-for(i in 1:k) {
-  title <- sprintf("Pattern - %d", i)
-
-  # 각 클러스터의 중점 패턴을 그린다 (대표 패턴)
-  plot(cl$centers[i, ], type = 'o', main = title, col = myCol[i], pch = 19)
-}
-
-library(ggplot2)
-
-ggplot(df, aes(x = code))+
-  geom_linerange(aes(ymin = Low, ymax = High)) +
-  theme_bw() +
-  labs(title = "df") +
-  geom_rect(aes(xmin = num - 0.2, xmax = num + 0.2, ymin = pmin(Open, Close), ymax = pmax(Open, Close)))
-
-
-
-##############################
-############### 저장하기
-
-###1### Export models data
-
-save(models, file = "price_scrape.rda")
-
+# library(dplyr)
+# library(parallel)
+# library(tictoc)
+# library(profvis)
+# library(htmlwidgets)
+#
+#
+# ##############################
+# ############### 변수 설정
+#
+# ## 코어수 감지
+# (core_num <- parallel::detectCores(all.tests = FALSE, logical = TRUE))
+#
+#
+# ## 데이터 load
+# setwd("P:/LICS/Rwd/Stock_analysis/data")
+# #setwd("C:/Users/user/Desktop/R project/Github/Stock_analysis/data")
+# load("all_stocks.rda")
+#
+#
+# ## INPUT data - X 생성
+# all_stocks$code %>% as.data.frame(stringsAsFactors = FALSE) ->.;
+# .[c(1:nrow(.)), ] -> datax
+# #.[c(1:100), ] -> datax
+#
+#
+# ## INDEX data - z 생성
+# zlevels <- 1:core_num
+# dataz <- if (sum(1:(length(datax) %% core_num)) == 1) {
+#   rep(1:core_num, length(datax) %/% core_num)
+# } else {
+#   c(rep(1:core_num, length(datax) %/% core_num), 1:(length(datax) %% core_num))
+# }
+#
+#
+# ##############################
+# ############### 함수 설정
+#
+#
+# ###1### WRAPPER 함수 생성
+# wrapper <- function(datax, dataz) {
+#
+#
+#   force(datax)
+#   force(dataz)
+#
+#
+#   ###2### 내용 함수 생성
+#   activation <- function(zlevel, datax, dataz) {
+#
+#
+#     ###3### Attach packages
+#     library(rebus, quietly = TRUE)
+#     library(rvest, quietly = TRUE)
+#
+#
+#     ###3### Define variables
+#     code_part <- datax[dataz == zlevel]
+#
+#
+#     ###3### Function body - real_time()
+#     real_time <- function(ticker) {
+#
+#       ticker ->.;
+#       paste("http://finance.daum.net/item/quote.daum?code=", ., sep = "") ->.;
+#       read_html(., encoding = "UTF-8") ->.;
+#       html_nodes(., "body") ->.;
+#       html_nodes(., "div") ->.;
+#       html_nodes(., ".leftDiv") ->.;
+#       html_nodes(., "td") ->.;
+#       html_text(.) %>%
+#         (function(df) {c("시가", "고가", "저가", "현재가", "거래량") %>%
+#             purrr::map(function(char) {
+#               char ->.; df[which(df == .) + 1] ->.; stringr::str_replace_all(., ",", "") ->.; as.numeric(.)
+#             })
+#         }) ->.;
+#       unlist(.) ->.;
+#       c(ticker, .)
+#
+#     } ###3### Last line of real_time()
+#
+#
+#     ###3### Function body - scale_cluster
+#     make_scale <- function(df) {
+#
+#       df[2:5] -> .
+#       as.numeric(.) ->.;
+#       log(.) ->.;
+#
+#       if (sum(is.infinite(.)) > 0) {
+#         c(0, 0, 0, 0) ->.;
+#         c(df[1], ., df[6]) ->.;
+#
+#       } else if (sum(is.na(.)) > 0) {
+#         c(0, 0, 0, 0) ->.;
+#         c(paste(df[1], "_X", sep = ""), ., df[6]) ->.;
+#
+#       } else if (sum(is.infinite(.)) == 0) {
+#         (. - .[1]) ->.;
+#         c(df[1], ., df[6]) ->.;
+#       }
+#
+#       as.data.frame(., stringsAsFactors = FALSE) ->.;
+#       t(.) ->.;
+#       {colnames(.) <- c("code", "Open", "High", "Low", "Close", "done_Q"); .} ->.;
+#       .
+#
+#     } ###3### Last line of scale_cluster()
+#
+#
+#     ###3### Function body - scale_kmeans
+#     scale_kmeans <- function(df, k) {
+#
+#       unlist(df, recursive = TRUE) ->.;
+#       matrix(., ncol = 6, byrow = TRUE) -> df2
+#
+#       df2[, c(2:5)] ->.;
+#       stats::kmeans(., centers = k, iter.max = 100, nstart = k) ->.;
+#
+#       cbind(df2[, c(2:5)], .$cluster, df2[, 6]) ->.;
+#       {colnames(.) <- c("Open", "High", "Low", "Close", "cluster", "done_Q"); .} ->.;
+#       apply(., 2, as.numeric) %>% as.data.frame(stringsAsFactors = FALSE) ->.;
+#       {.$cluster <- factor(.$cluster); .} ->.;
+#       {rownames(.) <- df2[, 1]; .} ->.;
+#       .
+#
+#     } ###3### Last line of scale_cluster()
+#
+#
+#     ###3### Activate the function with loop using map()
+#     code_part ->.;
+#     purrr::map(., function(ticker) {
+#
+#       real_time(ticker) ->.;
+#       make_scale(.)
+#
+#     }) ->.; ###3### Last line of code_part
+#
+#     scale_kmeans(., 2)
+#
+#   } ###2### Last line of Activation function()
+#
+#
+#   ####2####
+#   worker <- function(zlevel) {activation(zlevel, datax, dataz)}
+#
+#   return(worker)
+#
+# } ###1### Last line of price
+#
+#
+# ##############################
+# ############### 코어 활용 Scrape 시행 + 시간 체크
+#
+# tic("전체시간")
+# parallelcluster <- parallel::makeCluster(parallel::detectCores())
+#
+# tic("10개 크롤링")
+# models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
+# toc()
+#
+# parallel::stopCluster(parallelcluster)
+# toc()
+#
+#
+# ##############################
+# ############### 저장하기
+#
+# models %>% purrr::map(function(df) {
+#   df %>% tibble::rownames_to_column("code")
+# }) %>% rlist::list.flatten() %>% do.call(cbind, .) %>% as.data.frame %>%
+#   tibble::rownames_to_column("code") %>% (function(df) {df$num <- c(1:nrow(df)); df})
+#
+# k <- 8
+# cl <- stats::kmeans(df[, c(1:4)], centers = k, iter.max = 100, nstart = k)
+#
+# par(mfrow = c(2, k/2))
+# myCol <- c("blue", "red", "black", "brown", "green", "purple", "pink", "green")
+#
+# for(i in 1:k) {
+#   title <- sprintf("Pattern - %d", i)
+#
+#   # 각 클러스터의 중점 패턴을 그린다 (대표 패턴)
+#   plot(cl$centers[i, ], type = 'o', main = title, col = myCol[i], pch = 19)
+# }
+#
+# library(ggplot2)
+#
+# ggplot(df, aes(x = code))+
+#   geom_linerange(aes(ymin = Low, ymax = High)) +
+#   theme_bw() +
+#   labs(title = "df") +
+#   geom_rect(aes(xmin = num - 0.2, xmax = num + 0.2, ymin = pmin(Open, Close), ymax = pmax(Open, Close)))
+#
+#
+#
+# ##############################
+# ############### 저장하기
+#
+# ###1### Export models data
+#
+# save(models, file = "price_scrape.rda")
+#
+# # tic("저장시간")
+# #
+# # setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
+# # Sys.setenv("R_ZIPCMD" = "C:/Rtools/bin/zip.exe")
+# #
+# # for (j in 1:core_num) {
+# #
+# #   core_codes <- datax[dataz == j]
+# #   core_nms <- all_stocks$name[dataz == j]
+# #
+# #   for (i in 1:length(core_nms)) {
+# #     rm(wb)
+# #     wb <- openxlsx::createWorkbook()
+# #     addWorksheet(wb, core_codes[i])
+# #     writeData(wb,
+# #               sheet = core_codes[i],
+# #               models[[j]][[i]] %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column("date"),
+# #               startCol = 1, startRow = 1)
+# #     openxlsx::saveWorkbook(wb, paste(core_nms[i], ".xlsx", sep = ""), overwrite = TRUE)
+# #   }
+# # }
+# #
+# # toc()
+#
+#
+# ##############################
+# ############### 엑셀 파일 불러오기
+#
+# ###1### Import models data.xlsx
+#
 # tic("저장시간")
 #
 # setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
-# Sys.setenv("R_ZIPCMD" = "C:/Rtools/bin/zip.exe")
+# dir_nms <- dir()
 #
-# for (j in 1:core_num) {
 #
-#   core_codes <- datax[dataz == j]
-#   core_nms <- all_stocks$name[dataz == j]
+# ## Create empty models list
+# daily_candle_multi <- vector("list", length(dir_nms))
 #
-#   for (i in 1:length(core_nms)) {
-#     rm(wb)
-#     wb <- openxlsx::createWorkbook()
-#     addWorksheet(wb, core_codes[i])
-#     writeData(wb,
-#               sheet = core_codes[i],
-#               models[[j]][[i]] %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column("date"),
-#               startCol = 1, startRow = 1)
-#     openxlsx::saveWorkbook(wb, paste(core_nms[i], ".xlsx", sep = ""), overwrite = TRUE)
-#   }
+#
+# ## Import excel files
+# for (i in 1:length(dir_nms)) {
+#   readxl::read_excel(dir_nms[i], col_types = rep("text", 6)) ->.;
+#   xts::xts(.[, -1] %>% apply(2, as.numeric), order.by = zoo::as.Date(.$date)) %>%
+#     (function(df) {list(code = which(dir_nms[i] %>% stringr::str_replace_all(".xlsx", "") == all_stocks$name) %>% all_stocks$code[.],
+#                         price = df)
+#     }) -> models[[i]]
 # }
 #
+# names(models) <- dir_nms %>% stringr::str_replace_all(".xlsx", "")
+# save(models, file = "price_scrape.rda")
+#
 # toc()
-
-
-##############################
-############### 엑셀 파일 불러오기
-
-###1### Import models data.xlsx
-
-tic("저장시간")
-
-setwd("P:/LICS/Rwd/Stock_analysis/stock_price")
-dir_nms <- dir()
-
-
-## Create empty models list
-daily_candle_multi <- vector("list", length(dir_nms))
-
-
-## Import excel files
-for (i in 1:length(dir_nms)) {
-  readxl::read_excel(dir_nms[i], col_types = rep("text", 6)) ->.;
-  xts::xts(.[, -1] %>% apply(2, as.numeric), order.by = zoo::as.Date(.$date)) %>%
-    (function(df) {list(code = which(dir_nms[i] %>% stringr::str_replace_all(".xlsx", "") == all_stocks$name) %>% all_stocks$code[.],
-                        price = df)
-    }) -> models[[i]]
-}
-
-names(models) <- dir_nms %>% stringr::str_replace_all(".xlsx", "")
-save(models, file = "price_scrape.rda")
-
-toc()
-
-
-##############################
-############### 프로파일링 - profvis 패키지 활용
-
-# model_profvis <- profvis({
 #
-#   parallelcluster <- parallel::makeCluster(parallel::detectCores())
 #
-#   models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
+# ##############################
+# ############### 프로파일링 - profvis 패키지 활용
 #
-#   parallel::stopCluster(parallelcluster)
-#
-#   })
-#
-# saveWidget(widget = model_profvis, file = "model_profvis.html")
+# # model_profvis <- profvis({
+# #
+# #   parallelcluster <- parallel::makeCluster(parallel::detectCores())
+# #
+# #   models <- parallel::parLapply(parallelcluster, zlevels, wrapper(datax, dataz))
+# #
+# #   parallel::stopCluster(parallelcluster)
+# #
+# #   })
+# #
+# # saveWidget(widget = model_profvis, file = "model_profvis.html")
